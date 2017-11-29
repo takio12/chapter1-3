28474d73bde22adcc743275af15f78b64fecbac6
TAKIO-PC\takio <taka21centry@gmail.com>
2017-11-30 01:04:26 +0900

    とりあえず

--- chapter1-3/Program.cs
+++ chapter1-3/Program.cs
@@ -12,4 +12,23 @@ namespace chapter1_3
         {
         }
     }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public int GetAge()
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - Birthday.Year;
+            if (today < Birthday.AddYears(age))
+                age--;
+            return age;
+        }
+    }
+    public class Studend : Person
+    {
+        public int Grade { get; set; }
+        public int ClassNumber { get; set; }
+    }
 }
